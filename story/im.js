638d5646cb7a663f5275aa0874d9ee97b601ed9c
Kunal Kadam <techriderkk@gmail.com>
2018-02-18 17:28:59 +0530

    Preloading
    Preloading using image array

--- story/im.js
+++ story/im.js
@@ -1,9 +1,44 @@
+
 var i=486; /*imge strt from 486 to 3280 total 2794*/
-//var d;
-
-
+var d;
+var j=486;
+var images = new Array();
 
 /*FOR KEYBOARD USERS*/
+
+/*For Preloading*/
+
+    for(j=486;j<3280;j++){
+        images[j] = new Image();
+        images[j].src= 'star/Starless-Night-YouTube-'+j+'.jpg';
+    }
+j=486;
+//using array
+function funimg(e){
+    if(e.keyCode==39){
+        
+            if(j<3280){
+                j=j+1;
+                var img = document.getElementById('m').src= images[j].src;
+                  
+            }
+        
+    }
+    else if(e.keyCode==37){
+        var img = document.getElementById('m');
+
+            if(j>486){
+                j=j-1;
+                img.src= images[j].src; 
+            }
+        
+    }
+    
+}
+
+
+
+/*Original Working
 function funimg(e){
     if(e.keyCode==39){
         var img = document.getElementById('m');
@@ -26,7 +61,7 @@ function funimg(e){
     
 }
 
-
+*/
 /* FOR MOBILE USERS OR ONSCREEN ARROW USERS------ end comment here to work further
 function keyFwd(e){
     e.preventDefault();
